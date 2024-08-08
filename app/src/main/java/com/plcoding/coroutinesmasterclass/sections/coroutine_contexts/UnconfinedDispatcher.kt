ca84e06b15f062a858d54bab4a390694249c8bc7
Philipp Lackner <mail@pl-coding.com>
2024-08-08 17:10:58 +0200

    Unconfined Dispatcher

--- app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_contexts/UnconfinedDispatcher.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_contexts/UnconfinedDispatcher.kt
@@ -1,5 +1,9 @@
+@file:OptIn(DelicateCoroutinesApi::class)
+
 package com.plcoding.coroutinesmasterclass.sections.coroutine_contexts
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -11,7 +15,7 @@ fun unconfinedDispatcher() {
             println("Thread: ${Thread.currentThread().name}")
             withContext(Dispatchers.Unconfined) {
                 println("Thread: ${Thread.currentThread().name}")
-                withContext(Dispatchers.Default) {
+                withContext(Dispatchers.IO) {
                     println("Thread: ${Thread.currentThread().name}")
                 }
                 println("Thread: ${Thread.currentThread().name}")

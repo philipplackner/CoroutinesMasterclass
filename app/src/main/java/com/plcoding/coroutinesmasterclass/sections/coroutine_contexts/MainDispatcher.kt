3f1832dd0a140e06d0d153b1e0be29eaf8605225
Philipp Lackner <mail@pl-coding.com>
2024-08-08 17:04:36 +0200

    Main Dispatcher

--- app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_contexts/MainDispatcher.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_contexts/MainDispatcher.kt
@@ -1,12 +1,10 @@
 package com.plcoding.coroutinesmasterclass.sections.coroutine_contexts
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 
 suspend fun mainDispatcher() {
     withContext(Dispatchers.Main) {
-        delay(2000L)
 
         withContext(Dispatchers.Main.immediate) {
 

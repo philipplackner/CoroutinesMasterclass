e1f4d410fafa917c5721e95d56422a30b7737580
Philipp Lackner <mail@pl-coding.com>
2024-08-09 14:31:40 +0200

    Consequences of cancellation

--- app/src/main/java/com/plcoding/coroutinesmasterclass/MainActivity.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/MainActivity.kt
@@ -6,21 +6,38 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.lifecycleScope
 import com.plcoding.coroutinesmasterclass.sections.coroutine_contexts.main_safety.BitmapCompressor
-import com.plcoding.coroutinesmasterclass.sections.coroutine_contexts.main_safety.PhotoPickerScreen
 import com.plcoding.coroutinesmasterclass.ui.theme.CoroutinesMasterclassTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val customScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        customScope.launch {
+            delay(2000L)
+            println("Job finished!")
+        }
+        lifecycleScope.launch {
+            delay(1000L)
+            customScope.coroutineContext.cancelChildren()
+            customScope.launch {
+                println("Hello World!")
+            }
+        }
 
         val compressor = BitmapCompressor(applicationContext)
 
         setContent {
             CoroutinesMasterclassTheme {
-                PhotoPickerScreen(compressor = compressor)
+//                PhotoPickerScreen(compressor = compressor)
             }
         }
     }

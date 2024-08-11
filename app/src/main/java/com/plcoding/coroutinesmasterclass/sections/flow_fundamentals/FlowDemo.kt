dbb41e0c06615abed54feec680a40b7a88f8b24e
Philipp Lackner <mail@pl-coding.com>
2024-08-11 15:27:43 +0200

    The structure of a launched Flow

--- app/src/main/java/com/plcoding/coroutinesmasterclass/sections/flow_fundamentals/FlowDemo.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/sections/flow_fundamentals/FlowDemo.kt
@@ -3,19 +3,20 @@ package com.plcoding.coroutinesmasterclass.sections.flow_fundamentals
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 fun flowDemo() {
-    GlobalScope.launch {
-        flow<Int> {
-            delay(1000L)
-            emit(1)
-            delay(2000L)
-            emit(2)
-            delay(3000L)
-            emit(3)
-        }.collect {
-            println("Current value: $it")
-        }
+    flow<Int> {
+        delay(1000L)
+        emit(1)
+        delay(2000L)
+        emit(2)
+        delay(3000L)
+        emit(3)
     }
+        .onEach {
+            println("Value emitted: $it")
+        }
+        .launchIn(GlobalScope)
 }

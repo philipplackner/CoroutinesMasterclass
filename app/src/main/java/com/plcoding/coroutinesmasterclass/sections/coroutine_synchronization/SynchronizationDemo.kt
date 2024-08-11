44cf4c6d722efbe36566748ee4f613131c617598
Philipp Lackner <mail@pl-coding.com>
2024-08-11 14:56:05 +0200

    Single thread dispatcher

--- app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_synchronization/SynchronizationDemo.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_synchronization/SynchronizationDemo.kt
@@ -1,42 +1,37 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package com.plcoding.coroutinesmasterclass.sections.coroutine_synchronization
 
+import android.graphics.Bitmap
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 fun synchronizationDemo() {
     val normalHashMap = hashMapOf<Int, Int>()
     val concurrentHashMap = ConcurrentHashMap<Int, Int>()
-    val concurrentHashMap2 = ConcurrentHashMap<Int, Int>()
 
-    val mutex1 = Mutex()
-    val mutex2 = Mutex()
-    val mutex3 = Mutex()
-
-    GlobalScope.launch(Dispatchers.IO) {
+    GlobalScope.launch(Dispatchers.IO.limitedParallelism(1)) {
         (1..100000).map {
             launch {
                 val random = Random.nextInt(1, 9)
 
-                mutex1.withLock {
-                    val concurrentCount = concurrentHashMap[random] ?: 0
-                    concurrentHashMap[random] = concurrentCount + 1
-                }
+                val concurrentCount = concurrentHashMap[random] ?: 0
+                concurrentHashMap[random] = concurrentCount + 1
 
-                mutex2.withLock {
-                    val concurrentCount2 = concurrentHashMap2[random] ?: 0
-                    concurrentHashMap2[random] = concurrentCount2 + 1
-                }
-
-                mutex3.withLock {
-                    val normalCount = normalHashMap[random] ?: 0
-                    normalHashMap[random] = normalCount + 1
-                }
+                val normalCount = normalHashMap[random] ?: 0
+                normalHashMap[random] = normalCount + 1
             }
         }.joinAll()
 
@@ -47,18 +42,23 @@ fun synchronizationDemo() {
                 println("$key: $count")
             }
 
-        println("Concurrent hashmap1:")
+        println("Concurrent hashmap:")
         concurrentHashMap
             .toSortedMap()
             .forEach { (key, count) ->
                 println("$key: $count")
             }
+    }
+}
 
-        println("Concurrent hashmap2:")
-        concurrentHashMap2
-            .toSortedMap()
-            .forEach { (key, count) ->
-                println("$key: $count")
-            }
+class MySynchronizedClass(
+    private val coroutineScope: CoroutineScope =
+        CoroutineScope(Dispatchers.IO.limitedParallelism(1) + SupervisorJob())
+) {
+
+    fun doSomething() {
+        coroutineScope.launch {
+
+        }
     }
 }

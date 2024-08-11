0119cb33ba8d18a59d7aeea9092d2ac391a858ab
Philipp Lackner <mail@pl-coding.com>
2024-08-11 14:25:56 +0200

    Concurrent collections

--- app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_synchronization/SynchronizationDemo.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_synchronization/SynchronizationDemo.kt
@@ -6,19 +6,59 @@ import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.random.Random
 
 fun synchronizationDemo() {
-    var count = 0
-    val mutex = Mutex()
-    GlobalScope.launch {
-        (1..100_000).map {
+    val normalHashMap = hashMapOf<Int, Int>()
+    val concurrentHashMap = ConcurrentHashMap<Int, Int>()
+    val concurrentHashMap2 = ConcurrentHashMap<Int, Int>()
+
+    val mutex1 = Mutex()
+    val mutex2 = Mutex()
+    val mutex3 = Mutex()
+
+    GlobalScope.launch(Dispatchers.IO) {
+        (1..100000).map {
             launch {
-                mutex.withLock {
-                    count++
+                val random = Random.nextInt(1, 9)
+
+                mutex1.withLock {
+                    val concurrentCount = concurrentHashMap[random] ?: 0
+                    concurrentHashMap[random] = concurrentCount + 1
+                }
+
+                mutex2.withLock {
+                    val concurrentCount2 = concurrentHashMap2[random] ?: 0
+                    concurrentHashMap2[random] = concurrentCount2 + 1
+                }
+
+                mutex3.withLock {
+                    val normalCount = normalHashMap[random] ?: 0
+                    normalHashMap[random] = normalCount + 1
                 }
             }
         }.joinAll()
 
-        println("The count is $count")
+        println("Normal hashmap:")
+        normalHashMap
+            .toSortedMap()
+            .forEach { (key, count) ->
+                println("$key: $count")
+            }
+
+        println("Concurrent hashmap1:")
+        concurrentHashMap
+            .toSortedMap()
+            .forEach { (key, count) ->
+                println("$key: $count")
+            }
+
+        println("Concurrent hashmap2:")
+        concurrentHashMap2
+            .toSortedMap()
+            .forEach { (key, count) ->
+                println("$key: $count")
+            }
     }
 }

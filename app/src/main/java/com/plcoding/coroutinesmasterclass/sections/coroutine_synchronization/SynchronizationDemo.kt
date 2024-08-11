12b88d321d1ae9e201a8f5a54f3fb899aa9a548f
Philipp Lackner <mail@pl-coding.com>
2024-08-11 14:09:57 +0200

    synchronized vs. Mutex

--- app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_synchronization/SynchronizationDemo.kt
+++ app/src/main/java/com/plcoding/coroutinesmasterclass/sections/coroutine_synchronization/SynchronizationDemo.kt
@@ -4,13 +4,18 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 fun synchronizationDemo() {
     var count = 0
+    val mutex = Mutex()
     GlobalScope.launch {
         (1..100_000).map {
             launch {
-                count++
+                mutex.withLock {
+                    count++
+                }
             }
         }.joinAll()
 
